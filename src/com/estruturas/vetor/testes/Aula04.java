6f2fe1289cf4fb9d61e47e76a7f2a02b5885ded7
Edith Penha <edithpenha@gmail.com>
2022-01-22 15:32:40 -0400

    Estrutura de Dados - Vetor

---
+++ src/com/estruturas/vetor/testes/Aula04.java
@@ -0,0 +1,15 @@
+package com.estruturas.vetor.testes;
+
+import com.estruturas.vetor.Vetor;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+    }
+}
